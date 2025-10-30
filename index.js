a9b0e03ed4f43e97bf7d26dea9209d0f0c7f413f
Insany <insanyngame@gmail.com>
2025-10-29 21:34:13 -0300

    Added a simple room system.

--- index.js
+++ index.js
@@ -3,15 +3,58 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import ejs from 'ejs';
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
+var activeRooms = [];
+const roomRegex = /^[a-zA-Z0-9]{7}$/
+const userRegex = /^[a-zA-Z0-9_ ]+$/
+
+function validRoomCode(roomCode) {
+  if(!roomCode || !roomRegex.test(roomCode)) return false;
+  return true;
+}
+
+function roomExists(roomCode) {
+  if(!validRoomCode(roomCode)) return false;
+  return activeRooms.includes(roomCode);
+}
+
+function createRoom(roomCode) {
+  if(!validRoomCode(roomCode) || roomExists(roomCode)) return false;
+  activeRooms.push(roomCode);
+  return true;
+}
 
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+  res.render('index');
+});
+
+app.get('/create/:roomCode', (req, res) => {
+  console.log(req.params);
+  let roomCode = req.params.roomCode;
+  if(!validRoomCode(roomCode)) res.redirect('/?error=invalidroomcode');
+  else if(roomExists(roomCode)) res.redirect('/join/'+roomCode);
+  else {
+    createRoom(roomCode);
+    res.render("game", {roomCode: roomCode});
+    // res.sendFile(join(__dirname, 'index.html'));
+  }
+});
+
+app.get('/join/:roomCode', (req, res) => {
+  console.log(req.params);
+  let roomCode = req.params.roomCode;
+  if(!roomExists(roomCode)) res.redirect('/?error=roomnotfound');
+  else res.render("game", {roomCode: roomCode});
 });
 
 io.on('connection', (socket) => {
@@ -22,8 +65,14 @@ io.on('connection', (socket) => {
 });
 
 io.on('connection', (socket) => {
+
+ 
+  socket.on('enter room', (msg) => {
+    socket.emit("Entered room");
+  })
+
   socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+      io.emit('chat message', msg);
   });
 });
 
